a9e8eb69dbdcd51328dc85b56efd661c05729d36
shakib-mia <smdshakibmia2001@gmail.com>
2023-01-17 12:24:08 +0600

    forget password responsiveness done

--- src/pages/Frames/ForgotPassword.js
+++ src/pages/Frames/ForgotPassword.js
@@ -14,49 +14,51 @@ export default function ForgotPassword({
    const [forgotPassword, forgotPasswordResp] = useForgotPasswordMutation()
 
    const handleSubmit = () => {
-      forgotPassword({email: email })
-      .then(res => {
-         if(res.error){
-            console.log(res.error)
-            alert(res.error.data.message)
-         }
-         console.log(res.data);
-         window.open(res.data.link)
-      })
+      forgotPassword({ email: email })
+         .then(res => {
+            if (res.error) {
+               console.log(res.error)
+               alert(res.error.data.message)
+            }
+            console.log(res.data);
+            window.open(res.data.link)
+         })
    }
 
    return (
-      <div className="w-full px-148">
-         <p className="font-bold text-5xl leading-snug mb-7">
+      <div className="w-full">
+         <p className="font-bold text-[28px] py-[90px] pb-[34px] lg:text-5xl leading-snug mb-7 px-[49px] lg:px-148 bg-[#7152EB] lg:bg-transparent text-white lg:text-black">
             Forget Password
          </p>
 
-         <p
-            className="text-normal font-bold mb-90"
-            style={{ fontSize: "18px" }}
-         >
-            Enter your email address linked to this account s
-         </p>
+         <div className="px-[49px] lg:px-148">
+            <p
+               className="text-normal font-bold mb-90"
+               style={{ fontSize: "18px" }}
+            >
+               Enter your email address linked to this account s
+            </p>
 
-         <InputField
-            Icon={EmailIcon}
-            placeholder="Email address"
-            parentClassName="mb-6"
-            label="Email Address"
-            labelClassname="ml-2 mb-2"
-            inputClassName="bg-transparent"
-            inputContainerClassName='border'
-            onChange={(e) => setEmail(e.target.value)}
-         />
+            <InputField
+               Icon={EmailIcon}
+               placeholder="Email address"
+               parentClassName="mb-6"
+               label="Email Address"
+               labelClassname="ml-2 mb-2"
+               inputClassName="bg-transparent"
+               inputContainerClassName='border'
+               onChange={(e) => setEmail(e.target.value)}
+            />
 
-         <button
-            disabled={!emailValidate.test(email)}
-            className="w-full bg-primaryDark disabled:bg-pink py-4 rounded-10 text-white text-21"
-            // onClick={() => setActiveFrame(setResetPasswordActive)}
-            onClick={() => handleSubmit()}
-         >
-            Submit
-         </button>
+            <button
+               disabled={!emailValidate.test(email)}
+               className="w-full bg-primaryDark disabled:bg-pink py-2 lg:py-4 rounded-10 text-white text-21"
+               // onClick={() => setActiveFrame(setResetPasswordActive)}
+               onClick={() => handleSubmit()}
+            >
+               Submit
+            </button>
+         </div>
       </div>
    );
 }
