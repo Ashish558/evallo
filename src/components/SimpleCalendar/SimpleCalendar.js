65abd330e91ebb5e885c8042fa4ae94e7468bf76
Ashish558 <ashishprasad2001ap@gmail.com>
2023-03-07 18:02:31 +0530

    ui chamges in calendar done

--- src/components/SimpleCalendar/SimpleCalendar.js
+++ src/components/SimpleCalendar/SimpleCalendar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "./simpleCalendar.css";
@@ -11,6 +11,21 @@ export default function SimpleCalendar({ currentDate, setCurrentDate, events }) 
       setCurrentDate(e);
    };
 
+
+   useEffect(() => {
+      events.map(event => {
+         const date = new Date(event.start);  // 2009-11-10
+         const month = date.toLocaleString('default', { month: 'long' });
+         let str = `${month} ${date.getDate()}, ${date.getFullYear()}`
+         console.log(str);
+         var element = document.querySelector(`[aria-label="${str}"]`);
+         // console.log('element', element);
+         if(element !== null){
+            element.classList.add('calendar-dot')
+         }
+      })
+   }, [events])
+   
    return (
       <>
          <Calendar value={currentDate} onChange={changeDate}
