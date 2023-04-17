84d89d8684bbe4f1a768496ca63d4e3865e0445c
Ashish558 <ashishprasad2001ap@gmail.com>
2023-04-17 12:12:20 +0530

    calendar time input change
    change time input from time field to dropdown

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -17,4 +17,30 @@ export const subjects = [
    'American Literature', 'English', 'French', 'Latin', 'Writing / Composition', 'Algebra 1', 'Algebra 2', 'Geometry', 'Trigonometry', 'Statistics', 'Calculus', 'Biology', 'Chemistry', 'Physics', 'Economics', 'Geography',' US Government', 'US History', 'World History', 'Physical Education', 'Music', 'Drama', 'Photography', 'Comp Sci', 'Design', 'Business', 'Law', 'Psychology', 'Accounting'
 ]
 
+export const times = [
+   '12:00 AM',
+   '1:00 AM',
+   '2:00 AM',
+   '3:00 AM',
+   '4:00 AM',
+   '5:00 AM',
+   '6:00 AM',
+   '7:00 AM',
+   '8:00 AM',
+   '9:00 AM',
+   '10:00 AM',
+   '11:00 AM',
+   '12:00 PM',
+   '1:00 PM',
+   '2:00 PM',
+   '3:00 PM',
+   '4:00 PM',
+   '5:00 PM',
+   '6:00 PM',
+   '7:00 PM',
+   '8:00 PM',
+   '9:00 PM',
+   '10:00 PM',
+   '11:00 PM',
+]
 export const grades = [6, 7, 8, 9, 10, 11, 12, 'College'];
