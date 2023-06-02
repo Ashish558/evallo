eb811696b2a20ec286175a13407b8e825201ec43
Sushankznation <sushantbharwal@gmail.com>
2023-06-02 18:40:11 +0530

    Fixed Placeholder in SignUp

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -477,7 +477,7 @@ export default function Signup() {
                   </p>
                   <div className={`flex mt-[59px] lg:mt-0 ${styles.inputs}`}>
                     <InputField
-                      placeholder="First Name"
+                      placeholder="Vishesh"
                       parentClassName="text-xs"
                       label="First Name"
                       value={values.firstName}
@@ -490,7 +490,7 @@ export default function Signup() {
                       error={error.firstName}
                     />
                     <InputField
-                      placeholder="Last Name "
+                      placeholder="Patel"
                       parentClassName="text-xs"
                       label="Last Name"
                       value={values.lastName}
@@ -504,7 +504,7 @@ export default function Signup() {
                     />
                     <InputField
                       label="Work Email"
-                      placeholder="Work Email"
+                      placeholder=" Lorem123@gmail.com"
                       parentClassName="text-xs"
                       value={values.email}
                       onChange={(e) =>
@@ -516,7 +516,7 @@ export default function Signup() {
                       error={error.email}
                     />
                     <InputField
-                      placeholder="Phone"
+                      placeholder="99999994532"
                       parentClassName="text-xs"
                       label="Phone"
                       value={values.phone}
@@ -530,7 +530,7 @@ export default function Signup() {
                     />
 
                     <InputField
-                      placeholder="email@example.com"
+                      placeholder="Lorem"
                       parentClassName="text-xs mb-6"
                       label="Company"
                       value={values.company}
@@ -543,7 +543,7 @@ export default function Signup() {
                       error={error.company}
                     />
                     <InputField
-                      placeholder=""
+                      placeholder="Lorem"
                       parentClassName="text-xs mb-6"
                       label="Role"
                       value={values.role}
