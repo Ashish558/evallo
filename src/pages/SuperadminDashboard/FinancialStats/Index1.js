3d6686896dacf87feaca245e97a69959a315f34b
Shivam <shivam.yadav98939294@gmail.com>
2023-06-28 19:47:28 +0530

    made some little changes to the super admin dashboard page 1

--- src/pages/SuperadminDashboard/FinancialStats/Index1.js
+++ src/pages/SuperadminDashboard/FinancialStats/Index1.js
@@ -3,7 +3,7 @@ import FinancialStats from "./FinancialStats";
 
 export default function Index1() {
   return (
-    <div className="flex justify-center items-baseline h-40vh mt-8">
+    <div className="flex justify-center items-baseline h-40vh mt-8 gap-12">
       <FinancialStats />
       <ActionLog />
     </div>
