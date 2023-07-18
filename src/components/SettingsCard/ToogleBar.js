c16cb3e579a3756359c423b7c5fb01544002181f
Fahimphero <irfanulislamfahim@gmail.com>
2023-07-19 01:38:37 +0600

    org-default-page done

---
+++ src/components/SettingsCard/ToogleBar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import toggleRectIcon from "../../assets/icons/toggle-rect.svg";
+import toggleRectActiveIcon from "../../assets/icons/toggle-rect-active.svg";
+import toggleCircleIcon from "../../assets/icons/toggle-circle.svg";
+import styles from "./style.module.css";
+
+const ToogleBar = ({
+    title,
+    titleClassName,
+    className,
+    body,
+    toggle,
+    onToggle,
+}) => {
+    return (
+        <div
+            className={`rounded-2xl   ${className ? className : ""}
+          `}
+        >
+            <div
+                className={`text-primary-dark font-bold flex justify-between  ${titleClassName ? titleClassName : ""
+                    }`}
+            >
+                <p className="">{title}</p>
+                {toggle !== undefined && (
+                    <div className="flex items-center">
+                        {/* <p className="text-[#AAAAAA] text-xs mr-2">Show icons / images</p> */}
+                        <div
+                            className={styles.toggleContainer}
+                            onClick={() => onToggle(toggle.key, !toggle.value)}
+                        >
+                            <img
+                                src={
+                                    toggle.value === false ? toggleRectIcon : toggleRectActiveIcon
+                                }
+                                alt="toggle"
+                            />
+                            <img
+                                src={toggleCircleIcon}
+                                className={`${toggle.value === false
+                                    ? styles.toggleCircle
+                                    : styles.toggleCircleActive
+                                    }`}
+                                alt="toggle"
+                            />
+                        </div>
+                    </div>
+                )}
+            </div>
+            {body && body}
+        </div>
+    );
+};
+
+export default ToogleBar;
