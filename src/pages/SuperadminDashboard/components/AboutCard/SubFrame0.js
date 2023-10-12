c1f5c7aa002192ad84e2c6d1f016c566652cb64a
Fahimphero <irfanulislamfahim@gmail.com>
2023-10-13 03:38:23 +0600

    bug-fixed

--- src/pages/SuperadminDashboard/components/AboutCard/SubFrame0.js
+++ src/pages/SuperadminDashboard/components/AboutCard/SubFrame0.js
@@ -7,69 +7,69 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const SubFrame0 = ({ userData }) => {
-  const [fetchTotalhours, status]= useGetTotalHoursMutation()
-  const [totalHours,setTotalHours]= useState({})
- 
-  useEffect(()=>{
-  fetchTotalhours({orgId:userData.associatedOrg  }).then((res)=>{
-    console.log(res)
-    setTotalHours(res?.data)
-  }).catch(err =>{
- console.log(err)
-  })
-  },[userData])
+  const [fetchTotalhours, status] = useGetTotalHoursMutation()
+  const [totalHours, setTotalHours] = useState({})
+
+  useEffect(() => {
+    fetchTotalhours({ orgId: userData.associatedOrg }).then((res) => {
+      console.log(res)
+      setTotalHours(res?.data)
+    }).catch(err => {
+      console.log(err)
+    })
+  }, [userData])
   return (
     <div className="flex flex-col gap-5 ">
-      <div className="flex flex-col gap-4 border-b-[1.5px] border-b-gray-300">
-        <div className="flex  justify-between  p-2 px-3 text-sm  text-[#517CA8]">
-          <div className="">
-            <span className="w-[100px] mr-5">Org Type:</span>
-            <span className="font-semibold">{userData?.companyType}</span>
+      <div className="">
+        <div className="grid gap-x-[17%] gap-y-[20%] grid-cols-3  ml-[5%] mr-[9%]  text-base-18 text-[#517CA8] mt-[42px] ">
+          <div className="text-left">
+            <span className=" pr-3 font-light">Org Type:</span>
+            <span className="">{userData?.companyType}</span>
           </div>
-          <div className="">
-            <span className="w-[100px] mr-5">Location:</span>
-            <span className="font-semibold">
+          <div className="text-left font-light">
+            <span className="   pr-3">Location: </span>
+            <span className="">
               {userData?.city}, {userData?.state}, {userData?.country}
             </span>
           </div>
-          <div className="">
-            <span className="w-[100px] mr-5">Address:</span>
-            <span className="font-semibold">{userData?.address}</span>
+          <div className="text-left font-light">
+            <span className="   pr-3">Address:</span>
+            <span className="">{userData?.address}</span>
           </div>
-        </div>
-        <div className="flex gap-10  p-2 px-3 text-sm  text-[#517CA8]">
-          <div className="">
-            <span className="w-[100px] break-words mr-5">Year founded:</span>
-            <span className="font-semibold">
+          <div className="text-left font-light">
+            <span className="pr-3">Year founded:</span>
+            <span className="">
               {new Date(userData?.createdAt).getFullYear()}
             </span>
           </div>
-          <div className="">
-            <span className="w-[100px] mr-5">Services provides:</span>
-            <span className="font-semibold">It service</span>
+          <div className="text-left col-span-2 font-light">
+            <span className="pr-3">Services provides:</span>
+            <span className="">
+              IT service
+            </span>
           </div>
-        </div>
-        <div className="flex gap-10  p-2 px-3 text-sm  text-[#517CA8]">
-          <div className="">
-            <span className="w-[100px] break-words mr-5">Website:</span>
-            <span className="font-semibold">{userData?.website}</span>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col gap-4 border-b-[1.5px] border-b-gray-300">
-        <div className="flex  justify-between  p-2 px-3 text-sm  text-[#517CA8]">
-          <div className="">
-            <span className="w-[100px] mr-5">Subscription:</span>
-            <span className="font-semibold">Taken</span>
-          </div>
-        </div>
-        <div className="flex gap-10  p-2 px-3 text-sm  text-[#517CA8]">
-          <div className="">
-            <span className="w-[100px] mr-5">Status:</span>
-            <span className="font-semibold">{userData?.userStatus}</span>
+          <div className="text-left font-light">
+            <span className="pr-3">Website:</span>
+            <span className="">{userData?.website}</span>
           </div>
         </div>
       </div>
+      <div className="h-[1.33px] bg-gray-300 w-full mt-[35px]"></div>
+      <div className="flex flex-col gap-4  pl-[5%]">
+        <div className="flex  justify-between  p-2 px-3 text-base-18  text-[#517CA8]">
+          <div className="">
+            <span className=" mr-3 font-light">Subscription:</span>
+            <span className="">Taken</span>
+          </div>
+        </div>
+        <div className="flex gap-10  p-2 px-3 text-base-18  text-[#517CA8]">
+          <div className="">
+            <span className=" mr-3 font-light">Status:</span>
+            <span className="">{userData?.userStatus}</span>
+          </div>
+        </div>
+      </div>
+      <div className="h-[1.33px] bg-gray-300 w-full"></div>
       <div className="flex gap-3 w-full overflow-x-auto custom-scroller-2">
         <div className="flex flex-col p-2 px-3 gap-2">
           <span className=" ">
@@ -83,7 +83,7 @@ const SubFrame0 = ({ userData }) => {
         </div>
         <div className="flex gap-2 ">
           {aboutCardContents.map((item, idx) => {
-            return <AboutCard   heading={item.heading} text={totalHours?totalHours[item.key]:''}/>;
+            return <AboutCard heading={item.heading} text={totalHours ? totalHours[item.key] : ''} />;
           })}
         </div>
         <div className="bg-[#26435F] flex-1 flex flex-col rounded-md p-2 text-white ">
@@ -95,17 +95,17 @@ const SubFrame0 = ({ userData }) => {
               value={""}
               inputContainerClassName="bg-[#26435F]  p-0 !m-0 rounded-lg"
               optionClassName="w-[50px] text-xs !px-0 !mr-0 !py-[1.5px]"
-              //   onChange={(e) =>
-              //     setValues({
-              //       ...values,
-              //       orgType: e,
-              //     })
-              //   }
-              //   error={error.orgType}
+            //   onChange={(e) =>
+            //     setValues({
+            //       ...values,
+            //       orgType: e,
+            //     })
+            //   }
+            //   error={error.orgType}
             />
           </span>
           <span className="mt-3 text-center font-semibold text-lg">
-          {totalHours?.totalPrice}
+            {totalHours?.totalPrice}
 
           </span>
         </div>
