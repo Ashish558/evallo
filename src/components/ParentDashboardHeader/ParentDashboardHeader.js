b40daf733a9b0870379640233674efbf843d8e8c
Fahimphero <irfanulislamfahim@gmail.com>
2023-10-17 04:13:19 +0600

    bug-fixed

--- src/components/ParentDashboardHeader/ParentDashboardHeader.js
+++ src/components/ParentDashboardHeader/ParentDashboardHeader.js
@@ -234,7 +234,7 @@ const ParentDashboardHeader = ({ selectedStudent, setSelectedStudent }) => {
               optionType="object"
               label={"Select Student "}
               labelClassname="text-[#26435F]  font-semibold text-base-20 mb-[5px]"
-              iconClass=" translate-y-[0px] ml-3"
+              iconClass=" translate-y-[0px] ml-2 mb-[6px]"
               parentClassName=""
               inputContainerClassName="pt-1 pb-1"
               optionData={associatedStudents.map((item) => ({
