a9f01f998c8293ff55bf9d47a1d3c34d43985884
Adityaverma4648 <adityaverma4648@gmail.com>
2023-11-18 15:56:32 +0530

    Admin-settings - 933

--- src/pages/Settings/Tabs/AccountOverview/AccountOverview.js
+++ src/pages/Settings/Tabs/AccountOverview/AccountOverview.js
@@ -10,10 +10,10 @@ import resetSendIcon from "../../../../assets/icons/teenyicons_shield-tick-solid
 import tooltipIcon from "../../../../assets/icons/octicon_stop-16.svg";
 import { useState } from "react";
 import Modal from "../../../../components/Modal/Modal";
-import './style.css'
+import "./style.css";
 import { useForgotPasswordMutation } from "../../../../app/services/auth";
-import userPic from '../../../../assets/icons/user_logo.png'
-import camera from '../../../../assets/icons/camera_logo.svg'
+import userPic from "../../../../assets/icons/user_logo.png";
+import camera from "../../../../assets/icons/camera_logo.svg";
 
 import {
   useLazyGetPersonalDetailQuery,
@@ -61,7 +61,7 @@ const AccountOverview = () => {
   const [updateEmail, setUpdateEmail] = useUpdateEmailMutation();
   const [userDetails, userDetailsStatus] = useLazyGetPersonalDetailQuery();
   const [updateAccount, updateAccountStatus] = useUpdateUserAccountMutation();
-  const [fetchedData, setFetchedData] = useState({})
+  const [fetchedData, setFetchedData] = useState({});
   useEffect(() => {
     userDetails()
       .then((res) => {
@@ -77,61 +77,64 @@ const AccountOverview = () => {
       });
   }, []);
 
-   const isEmail=(val)=> {
-    let regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const isEmail = (val) => {
+    let regEmail =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!regEmail.test(val)) {
-       return false
-    }else{
-       return true
+      return false;
+    } else {
+      return true;
     }
- }
+  };
   const handleEmailUpdate = (email) => {
     console.log("Email Updation invoked", email);
-    if(email?.trim()===""){
-      alert("Email address can't be empty")
-      return
+    if (email?.trim() === "") {
+      alert("Email address can't be empty");
+      return;
     }
-    if(!isEmail(email)){
-      alert("Enter valid email!")
-      return
+    if (!isEmail(email)) {
+      alert("Enter valid email!");
+      return;
     }
-    if (email?.trim() !== ""){
-    
-    
+    if (email?.trim() !== "") {
       updateEmail({ email }).then((res) => {
-    if(res?.data){
-      alert("Email reset link sent, please verify")
-
-    }
-    else {
-      alert("Error occured while senting email reset link!")
-    }
+        if (res?.data) {
+          alert("Email reset link sent, please verify");
+        } else {
+          alert("Error occured while senting email reset link!");
+        }
         console.log("Email Link sent", res);
       });
     }
   };
-  const [loading,setLoading]=useState(false)
+  const [loading, setLoading] = useState(false);
   const handleDataUpdate = () => {
-    const arr=["email","firstName","lastName","phone","phoneCode"]
-    let boo=true;
-    let ff=null
-    arr.forEach((it)=>{
-     if(boo&&(!values[it] || values[it]?.trim()==="")){
-        boo=false
-        alert(it+" can't be empty.")
-        return 
+    const arr = ["email", "firstName", "lastName", "phone", "phoneCode"];
+    let boo = true;
+    let ff = null;
+    arr.forEach((it) => {
+      if (boo && (!values[it] || values[it]?.trim() === "")) {
+        boo = false;
+        alert(it + " can't be empty.");
+        return;
+      }
+    });
+    if (!boo) {
+      return;
     }
-  })
-  if(!boo){
-    return 
-  }
-  if(!isEmail(values?.email)){
-    alert("Enter valid email!")
-    return
-  }
-    if(values?.email?.trim()===""||values?.firstName?.trim()===""||values?.lastName?.trim()===""||values?.phone?.trim()===""||values?.phoneCode?.trim()===""){
-      alert("Please fill all the fields to update your account!")
-      return 
+    if (!isEmail(values?.email)) {
+      alert("Enter valid email!");
+      return;
+    }
+    if (
+      values?.email?.trim() === "" ||
+      values?.firstName?.trim() === "" ||
+      values?.lastName?.trim() === "" ||
+      values?.phone?.trim() === "" ||
+      values?.phoneCode?.trim() === ""
+    ) {
+      alert("Please fill all the fields to update your account!");
+      return;
     }
     setLoading(true);
     const updateUserAccount = async () => {
@@ -140,14 +143,13 @@ const AccountOverview = () => {
         delete reqBody["_id"];
         delete reqBody["email"];
         updateAccount(reqBody)
-
           .then((res) => {
             setLoading(false);
-            if(res?.error){
-              alert("Error occured while updating!")
+            if (res?.error) {
+              alert("Error occured while updating!");
             }
-            if(res?.data){
-              alert("changes saved!")
+            if (res?.data) {
+              alert("changes saved!");
             }
             console.log(res);
           })
@@ -160,9 +162,8 @@ const AccountOverview = () => {
       }
     };
     updateUserAccount();
-    if (fetchedData?.email !== values.email)
-      handleEmailUpdate(values.email)
-    console.log({ fetchedData, values })
+    if (fetchedData?.email !== values.email) handleEmailUpdate(values.email);
+    console.log({ fetchedData, values });
   };
   const showResetConfirmation = () => {
     setReset(true);
@@ -199,105 +200,108 @@ const AccountOverview = () => {
   return (
     <div>
       <div className="flex flex-col gap-10  ">
-        <div className="flex gap-8">
-          <InputField
-            placeholder=""
-            labelClassname="font-medium text-base"
-            parentClassName="text-[#26435F]"
-            inputContainerClassName=" shadow-[0px_0px_2.500000476837158px_0px_#00000040]  bg-white  border border-white text-[#667085]"
-            inputClassName=" text-400 py-0 bg-transparent"
-            label="First name"
-            value={values.firstName}
-            onChange={(e) =>
-              setValues({
-                ...values,
-                firstName: e.target.value,
-              })
-            }
-            error={error.firstName}
-          />
-
-          <InputField
-            placeholder=""
-            labelClassname="font-medium text-base"
-            parentClassName="text-[#26435F]"
-            inputContainerClassName=" shadow-[0px_0px_2.500000476837158px_0px_#00000040]  bg-white border border-white text-[#667085]"
-            inputClassName=" text-400 py-0 bg-transparent"
-            label="Last name"
-            value={values.lastName}
-            onChange={(e) =>
-              setValues({
-                ...values,
-                lastName: e.target.value,
-              })
-            }
-            error={error.lastName}
-          />
-
-          <InputField
-            IconLeft={caution}
-            placeholder=""
-            labelClassname="font-medium text-base"
-            parentClassName="text-[#26435F]"
-            inputContainerClassName=" !shadow-[0px_0px_2.500000476837158px_0px_#00000040]  bg-white  text-[#667085]"
-            inputClassName=" text-400 py-0 bg-transparent w-[calc(377*0.0522vw)]"
-            label="Email"
-            // IconRight={tooltipIcon}
-            value={values.email}
-            onChange={(e) => {
-              setValues({
-                ...values,
-                email: e.target.value,
-              });
-
-            }}
-            error={error.email}
-            Tooltip={
-              <span className="absolute top-10 w-[333px] h-[200px] scale-0 rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100">
-                <h3 className="text-[#24A3D9] font-semibold mb-1">
-                  Email Confirmation Sent
-                </h3>
-                You need to verify your email if
-                <ul className="list-disc pl-3 mb-2">
-                  <li>you created a new account.</li>
-                  <li>you recently changed your email.</li>
-                </ul>
-                We have sent you an email verification link to your current
-                email address to make sure that it really is you who requested a
-                change.
-              </span>
-            }
-          />
-          <div id="number">
-            <InputFieldDropdown
+        <div className="gap-8  flex justify-between items-center">
+          <div className="w-4/5 flex justify-start items-center ">
+            <InputField
               placeholder=""
               labelClassname="font-medium text-base"
-              parentClassName="text-[#26435F] "
-              inputContainerClassName=" shadow-[0px_0px_2.500000476837158px_0px_#00000040] py-3 bg-white border border-white text-[#667085]"
-              inputClassName="  text-400 py-1"
-              label="Phone"
-              value={values.phone}
-              codeValue={values.phoneCode}
-              handleCodeChange={(e) =>
-                setValues({
-                  ...values,
-                  phoneCode: e.target.value,
-                })
-              }
+              parentClassName="text-[#26435F]"
+              inputContainerClassName=" shadow-[0px_0px_2.500000476837158px_0px_#00000040]  bg-white  border border-white text-[#667085] mr-[38px]"
+              inputClassName=" text-400 py-0 bg-transparent "
+              label="First name"
+              value={values.firstName}
               onChange={(e) =>
                 setValues({
                   ...values,
-                  phone: e.target.value,
+                  firstName: e.target.value,
                 })
               }
-              error={error.phone}
+              error={error.firstName}
             />
+
+            <InputField
+              placeholder=""
+              labelClassname="font-medium text-base"
+              parentClassName="text-[#26435F]"
+              inputContainerClassName=" shadow-[0px_0px_2.500000476837158px_0px_#00000040]  bg-white border border-white text-[#667085] mr-[38px]"
+              inputClassName=" text-400 py-0 bg-transparent"
+              label="Last name"
+              value={values.lastName}
+              onChange={(e) =>
+                setValues({
+                  ...values,
+                  lastName: e.target.value,
+                })
+              }
+              error={error.lastName}
+            />
+
+            <InputField
+              IconLeft={caution}
+              placeholder=""
+              labelClassname="font-medium text-base"
+              parentClassName="text-[#26435F]"
+              inputContainerClassName=" !shadow-[0px_0px_2.500000476837158px_0px_#00000040]  bg-white  text-[#667085] mr-[38px]"
+              inputClassName=" text-400 py-0 bg-transparent w-[calc(377*0.0522vw)]"
+              label="Email"
+              // IconRight={tooltipIcon}
+              value={values.email}
+              onChange={(e) => {
+                setValues({
+                  ...values,
+                  email: e.target.value,
+                });
+              }}
+              error={error.email}
+              Tooltip={
+                <span className="absolute top-10 w-[333px] h-[200px] scale-0 rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100">
+                  <h3 className="text-[#24A3D9] font-semibold mb-1">
+                    Email Confirmation Sent
+                  </h3>
+                  You need to verify your email if
+                  <ul className="list-disc pl-3 mb-2">
+                    <li>you created a new account.</li>
+                    <li>you recently changed your email.</li>
+                  </ul>
+                  We have sent you an email verification link to your current
+                  email address to make sure that it really is you who requested
+                  a change.
+                </span>
+              }
+            />
+            <div id="number">
+              <InputFieldDropdown
+                placeholder=""
+                labelClassname="font-medium text-base"
+                parentClassName="text-[#26435F] "
+                inputContainerClassName="!shadow-[0px_0px_2.500000476837158px_0px_#00000040]  bg-white  text-[#667085]"
+                inputClassName="text-400 py-0 bg-transparent w-[calc(377*0.0522vw)]"
+                label="Phone"
+                value={values.phone}
+                codeValue={values.phoneCode}
+                handleCodeChange={(e) =>
+                  setValues({
+                    ...values,
+                    phoneCode: e.target.value,
+                  })
+                }
+                onChange={(e) =>
+                  setValues({
+                    ...values,
+                    phone: e.target.value,
+                  })
+                }
+                error={error.phone}
+              />
+            </div>
           </div>
           <div className="flex justify-start items-center">
             <button
-               disabled={loading}
+              disabled={loading}
               onClick={handleDataUpdate}
-              className={` bg-[#FFA28D]  py-[13.5px] mt-[20px] rounded-md px-10  text-sm text-[#fff] text-base-17-5 ${loading?"cursor-wait":"cursor-pointer"}`}
+              className={` bg-[#FFA28D] h-[50px] w-[175px]  py-[13.5px] mt-[20px] rounded-md px-10  text-sm text-[#fff] text-base-17-5 ${
+                loading ? "cursor-wait" : "cursor-pointer"
+              }`}
             >
               Save
             </button>
@@ -306,9 +310,15 @@ const AccountOverview = () => {
         <div className="flex gap-x-[30px]">
           <div>
             <div className="flex items-center relative">
-              <p className="text-base-17-5 font-semibold text-[#26435F] mr-5">Profile Picture</p>
-              <p ><img className=" inline-block" src={userPic} alt="" /></p>
-              <p className="absolute right-0 bottom-0"><img src={camera} alt="" /></p>
+              <p className="text-base-17-5 font-semibold text-[#26435F] mr-5">
+                Profile Picture
+              </p>
+              <p>
+                <img className=" inline-block" src={userPic} alt="" />
+              </p>
+              <p className="absolute right-0 bottom-0">
+                <img src={camera} alt="" />
+              </p>
             </div>
             <InputField
               placeholder="What is your role?"
@@ -329,7 +339,12 @@ const AccountOverview = () => {
           </div>
           <div className="flex items-end">
             <div className="flex flex-col w-[35.78vw]">
-              <label className="text-[#26435F] font-semibold text-base-17-5" htmlFor="">Short Bio</label>
+              <label
+                className="text-[#26435F] font-semibold text-base-17-5"
+                htmlFor=""
+              >
+                Short Bio
+              </label>
               <textarea
                 placeholder="Write a short bio about yourself, your interests, how your got started with this choice of career, your strengths and weaknesses, your hobbies, your tutoring style, your educational background, etc. Suggested word length: 200 words."
                 rows={7}
@@ -341,11 +356,11 @@ const AccountOverview = () => {
                 }}
                 value={values.about}
                 onChange={(e) => {
-                setValues({
-                  ...values,
-                about: e.target.value,
-                })
-              }}
+                  setValues({
+                    ...values,
+                    about: e.target.value,
+                  });
+                }}
               ></textarea>
             </div>
             <div className="ml-[38px]">
@@ -368,7 +383,6 @@ const AccountOverview = () => {
               Download
             </button>
           </div> */}
-
         </div>
         <div>
           {reset && (
@@ -387,11 +401,11 @@ const AccountOverview = () => {
           classname="!w-[666px] mx-auto"
           underline={true}
           titleClassName="m-0"
-         
           buttonParentClassName="justify-center"
           primaryBtn={{
             text: "Okay",
-            className: "pl-4 px-4 !bg-[#FF7979] text-white w-[146px] h-[46px] !ml-0",
+            className:
+              "pl-4 px-4 !bg-[#FF7979] text-white w-[146px] h-[46px] !ml-0",
             onClick: () => showResetConfirmation(),
             // disabled: submitBtnDisabled,
             // loading: loading
@@ -400,7 +414,7 @@ const AccountOverview = () => {
           body={
             <div className="text-center mb-[30px]">
               <h1 className="text-[21px] text-[#26435F]">
-                A Password Reset Link will be sent to you. Please click on it to change your password.
+                A Password Reset Link Will Be Sent To You. Please Click On It To Change Your Password.
               </h1>
               {/* <button
                 onClick={showResetConfirmation}
