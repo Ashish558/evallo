315c123f528621faf4ee519da8bbf49e77a9d599
Ashish558 <ashishprasad2001ap@gmail.com>
2023-12-07 18:27:21 +0530

    settings bug fix

--- src/components/BarGraph/BarGraph.js
+++ src/components/BarGraph/BarGraph.js
@@ -3,15 +3,16 @@ import ReactApexChart from 'react-apexcharts'
 
 export default function BarGraph({ series, options, height, width }) {
 
+   // console.log('timeSeries', series);
    return (
-      
+
       <div>
          <ReactApexChart options={options}
             height={height ? height : '700px'}
             series={series}
             type='bar'
-            width={width ? width :'100%'}
-           />
+            width={width ? width : '100%'}
+         />
       </div>
    )
 }
