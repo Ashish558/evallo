f93012c1d771d0b33d29284f264fe7f7d202cc09
BRIJESH <122820661+Brijesh0707@users.noreply.github.com>
2023-12-12 00:19:35 +0530

    admincontent

--- src/components/Table/tableHeaderObj.js
+++ src/components/Table/tableHeaderObj.js
@@ -22,7 +22,7 @@ export function TableHeaderNew({
   return dataFor === "assignedTests" ? (<th className={`pt-[26.25px] pb-[24.25px] text-white text-[17.5px] leading-3 font-medium ${header.text === "Due On" || header.text === "Completion" || header.text === "Score" ? "text-center" : " text-left pl-[30px]"}`}>
     <div className={``}>{header.text}</div>
   </th>):
-  dataFor==="allTests"?(<th className={`pt-[26.25px] pb-[24.25px] text-white text-[17.5px] leading-[25px] font-medium ${header.text==="Assignment Name"?"text-left pl-[65.6px]":header.text==="Total Assignments"?"text-center":" text-left"}`}>
+  dataFor==="allTests"?(<th className={`pt-[26.25px] pb-[24.25px] text-white text-[17.5px] leading-[25px] font-medium ${header.text==="Assignment Name"?"text-left pl-[28px]":header.text==="Total Assignments"?"text-center":" text-left"}`}>
   <div onClick={() => header.onCick && header.onCick()} className={`flex items-center gap-0 ${header.text==="Total Assignments"&&"justify-center"}`}><p className={`${header.text==="Type"? 'pl-11':''} ${header.text==="Student Name"?'pl-10':''} `}>{header.text}</p>{
     header.noArrow?"":
      header.willDisplayDownArrow=== null||header.text.lenght===0?"":
