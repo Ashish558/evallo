5faf8f5307982352d30cc401a413afa217119cba
Ashish558 <ashishprasad2001ap@gmail.com>
2023-12-12 18:18:44 +0530

    student profile dashboard chart bug fix

--- src/pages/Profiles/StudentProfile/SPframes/SPFrame3.js
+++ src/pages/Profiles/StudentProfile/SPframes/SPFrame3.js
@@ -12,7 +12,7 @@ import RangeDate from "../../../../components/RangeDate/RangeDate";
 import InputSelectNew from "../../../../components/InputSelectNew/InputSelectNew";
 import { useParams } from "react-router-dom";
 import StudentBubbleChart from "./StudentNewBubbleChart";
-const SPFrame3 = ({ userDetail, isOwn, user }) => {
+const SPFrame3 = ({ userDetail, isOwn, user, displayScoreProgressOnly, chartContainerClassName, chartClassName }) => {
   const [getProgression, Progstatus] = useScoreProgressionStudentMutation();
   const [getBubbleChart, bubbleChartStatus] = useChartBubbleStudentMutation();
 
@@ -163,16 +163,11 @@ const SPFrame3 = ({ userDetail, isOwn, user }) => {
     });
     setAccSubject(updated);
   };
-  console.log("spframe3", {
-    accsubjects,
-    selectedspSubject,
-    spSubject,
-    scoreProgression,
-  });
+
   return (
     <div className="flex flex-col gap-5 -mt-5 design:gap-10">
       {" "}
-      <h1 className="text-[#26435F] translate-y-8 text-sm font-semibold cursor-pointer  text-[20px] pb-1 flex items-center justify-start">
+      <h1 className="text-[#26435F] translate-y-8 font-semibold cursor-pointer  text-[20px] pb-1 flex items-center justify-start">
         Score progression
         <div className="inline-block my-auto ml-[18.75px] ">
           <svg
@@ -191,7 +186,7 @@ const SPFrame3 = ({ userDetail, isOwn, user }) => {
       </h1>
       <div
         id={styles.chartContainer}
-        className="!rounded-md bg-white shadow-[0px_0px_2.500001907348633px_0px_#00000040] custom-scroller mt-4  pl-5"
+        className={`!rounded-md bg-white shadow-[0px_0px_2.500001907348633px_0px_#00000040] custom-scroller mt-4 pl-5 ${chartContainerClassName ? chartContainerClassName : ''} `}
       >
         {" "}
         <div className="flex-1 relative z-[6000] flex justify-end translate-y-4 mr-3">
@@ -219,7 +214,7 @@ const SPFrame3 = ({ userDetail, isOwn, user }) => {
             /> */}
         </div>
         {scoreProgression && scoreProgression?.length > 0 ? (
-          <LineChart scoreProgression={scoreProgression} />
+          <LineChart scoreProgression={scoreProgression} chartClassName={chartClassName} />
         ) : (
           <div
             id="ssprogress"
@@ -238,46 +233,49 @@ const SPFrame3 = ({ userDetail, isOwn, user }) => {
           </div>
         )}
       </div>
-      <div className=" flex-1 w-full">
-        <div className="flex items-center justify-between">
-          <h1 className="text-[#26435F]  text-sm font-semibold mb-1 text-[20px] pb-1  flex items-center justify-start">
-            Time Management
-            <div className="inline-block my-auto ml-[18.75px] cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 19 19"
-                fill="none"
-              >
-                <path
-                  d="M18.2844 9.64347C18.2844 14.5545 14.3032 18.536 9.39219 18.536C4.48115 18.536 0.5 14.5551 0.5 9.64347C0.5 4.73213 4.48086 0.750977 9.39219 0.750977C14.3032 0.750977 18.2844 4.73243 18.2844 9.64347ZM9.39219 2.73877C5.57862 2.73877 2.48721 5.83018 2.48721 9.64376C2.48721 13.457 5.57832 16.549 9.39219 16.549C13.2058 16.549 16.2972 13.457 16.2972 9.64376C16.2975 5.83047 13.2058 2.73877 9.39219 2.73877ZM7.57198 7.57716C7.57198 6.17442 8.47168 5.68194 9.3708 5.68194C9.90606 5.68194 10.3643 5.73145 10.7305 6.18497C11.5871 7.24522 10.6312 8.27413 9.5419 8.81876C8.7711 9.20431 8.40694 9.85763 8.40694 10.7673V12.8023H10.441V11.6028C10.441 9.77179 11.6188 10.232 12.4754 9.08683C13.6338 7.53761 13.3833 5.79122 11.8112 4.54698C10.013 3.12285 7.97803 3.8295 7.41084 4.21504C6.93946 4.53526 5.26953 5.72471 5.59063 8.2835L7.56729 8.28761C7.56729 8.28761 7.57139 8.04825 7.57139 7.57716H7.57198ZM10.4416 15.5861V13.4984H8.40752V15.5861H10.4416Z"
-                  fill="#26435F"
-                />
-              </svg>
+      {
+        !displayScoreProgressOnly &&
+        <>
+          <div className=" flex-1 w-full">
+            <div className="flex items-center justify-between">
+              <h1 className="text-[#26435F]  text-sm font-semibold mb-1 text-[20px] pb-1  flex items-center justify-start">
+                Time Management
+                <div className="inline-block my-auto ml-[18.75px] cursor-pointer">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 19 19"
+                    fill="none"
+                  >
+                    <path
+                      d="M18.2844 9.64347C18.2844 14.5545 14.3032 18.536 9.39219 18.536C4.48115 18.536 0.5 14.5551 0.5 9.64347C0.5 4.73213 4.48086 0.750977 9.39219 0.750977C14.3032 0.750977 18.2844 4.73243 18.2844 9.64347ZM9.39219 2.73877C5.57862 2.73877 2.48721 5.83018 2.48721 9.64376C2.48721 13.457 5.57832 16.549 9.39219 16.549C13.2058 16.549 16.2972 13.457 16.2972 9.64376C16.2975 5.83047 13.2058 2.73877 9.39219 2.73877ZM7.57198 7.57716C7.57198 6.17442 8.47168 5.68194 9.3708 5.68194C9.90606 5.68194 10.3643 5.73145 10.7305 6.18497C11.5871 7.24522 10.6312 8.27413 9.5419 8.81876C8.7711 9.20431 8.40694 9.85763 8.40694 10.7673V12.8023H10.441V11.6028C10.441 9.77179 11.6188 10.232 12.4754 9.08683C13.6338 7.53761 13.3833 5.79122 11.8112 4.54698C10.013 3.12285 7.97803 3.8295 7.41084 4.21504C6.93946 4.53526 5.26953 5.72471 5.59063 8.2835L7.56729 8.28761C7.56729 8.28761 7.57139 8.04825 7.57139 7.57716H7.57198ZM10.4416 15.5861V13.4984H8.40752V15.5861H10.4416Z"
+                      fill="#26435F"
+                    />
+                  </svg>
+                </div>
+              </h1>
             </div>
-          </h1>
-        </div>
-        <div
-          id={styles.chartContainer}
-          className="!rounded-md w-full bg-white flex-1 shadow-[0px_0px_2.500001907348633px_0px_#00000040] custom-scroller pb-5 pl-4"
-        >
-          {spSubject && spSubject?.length > 0 ? (
-            <>
-              <div className="flex-1 relative z-[6000] flex justify-end translate-y-4 mr-3">
-                <InputSelectNew
-                  placeholder={""}
-                  parentClassName="ml-0  scale-[0.9] items-center flex text-[#FFA28D] text-xs border px-1 py-2 border-[#FFA28D] rounded-full  "
-                  inputContainerClassName=" my-0 py-[5px] px-[35px]"
-                  placeHolderClass="text-[#FFA28D] "
-                  labelClassname="text-sm"
-                  inputClassName="bg-transparent"
-                  value={selectedspSubject}
-                  IconDemography={true}
-                  optionData={spSubject.map((item) => item.subject)}
-                  onChange={(e) => handlespSubjectChange(e)}
-                />
-                {/* <InputSelect
+            <div
+              id={styles.chartContainer}
+              className="!rounded-md w-full bg-white flex-1 shadow-[0px_0px_2.500001907348633px_0px_#00000040] custom-scroller pb-5 pl-4"
+            >
+              {spSubject && spSubject?.length > 0 ? (
+                <>
+                  <div className="flex-1 relative z-[6000] flex justify-end translate-y-4 mr-3">
+                    <InputSelectNew
+                      placeholder={""}
+                      parentClassName="ml-0  scale-[0.9] items-center flex text-[#FFA28D] text-xs border px-1 py-2 border-[#FFA28D] rounded-full  "
+                      inputContainerClassName=" my-0 py-[5px] px-[35px]"
+                      placeHolderClass="text-[#FFA28D] "
+                      labelClassname="text-sm"
+                      inputClassName="bg-transparent"
+                      value={selectedspSubject}
+                      IconDemography={true}
+                      optionData={spSubject.map((item) => item.subject)}
+                      onChange={(e) => handlespSubjectChange(e)}
+                    />
+                    {/* <InputSelect
               value={selectedSubject}
               labelClassname="hidden"
               parentClassName="w-fit mr-5"
@@ -286,27 +284,27 @@ const SPFrame3 = ({ userDetail, isOwn, user }) => {
               onChange={(val) => handleSubjectChange(val)}
             /> */}
 
-                <RangeDate
-                  removeUnderline={true}
-                  className="ml-0 !font-normal"
-                  optionClassName="!w-min"
-                  inputContainerClassName="!w-min font-normal"
-                  handleRangeData={handleTimeManagement}
-                />
-              </div>
-              <StudentBubbleChart
-                subjects={spSubject}
-                selectedSubject={selectedspSubject}
-                time={true}
-              />
-            </>
-          ) : (
-            <div
-              id="stime"
-              className=" w-full  z-[5000] min-h-[300px] rounded-md bg-white flex justify-center flex-col text-center items-center"
-            >
-              <div className="w-[70%] mx-auto   flex flex-col items-start">
-                {/* <button className="bg-[#FF7979] text-white rounded-md p-2 py-1 mb-3">
+                    <RangeDate
+                      removeUnderline={true}
+                      className="ml-0 !font-normal"
+                      optionClassName="!w-min"
+                      inputContainerClassName="!w-min font-normal"
+                      handleRangeData={handleTimeManagement}
+                    />
+                  </div>
+                  <StudentBubbleChart
+                    subjects={spSubject}
+                    selectedSubject={selectedspSubject}
+                    time={true}
+                  />
+                </>
+              ) : (
+                <div
+                  id="stime"
+                  className=" w-full  z-[5000] min-h-[300px] rounded-md bg-white flex justify-center flex-col text-center items-center"
+                >
+                  <div className="w-[70%] mx-auto   flex flex-col items-start">
+                    {/* <button className="bg-[#FF7979] text-white rounded-md p-2 py-1 mb-3">
                   No Assignments Yet
                 </button>
                 <p className=" !whitespace-normal !text-left">
@@ -314,51 +312,51 @@ const SPFrame3 = ({ userDetail, isOwn, user }) => {
                   assignment is given, the student will be able to start it and
                   view detailed score reports through this table.
                 </p> */}
-              </div>
+                  </div>
+                </div>
+              )}
             </div>
-          )}
-        </div>
-      </div>
-      <div className=" flex-1 w-full">
-        <div className="flex items-center justify-between">
-          <h1 className="text-[#26435F] text-sm font-semibold mb-1 text-[20px] pb-1  flex items-center justify-start">
-            Conceptual Accuracy 
-            <div className="inline-block my-auto ml-[18.75px] cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 19 19"
-                fill="none"
-              >
-                <path
-                  d="M18.2844 9.64347C18.2844 14.5545 14.3032 18.536 9.39219 18.536C4.48115 18.536 0.5 14.5551 0.5 9.64347C0.5 4.73213 4.48086 0.750977 9.39219 0.750977C14.3032 0.750977 18.2844 4.73243 18.2844 9.64347ZM9.39219 2.73877C5.57862 2.73877 2.48721 5.83018 2.48721 9.64376C2.48721 13.457 5.57832 16.549 9.39219 16.549C13.2058 16.549 16.2972 13.457 16.2972 9.64376C16.2975 5.83047 13.2058 2.73877 9.39219 2.73877ZM7.57198 7.57716C7.57198 6.17442 8.47168 5.68194 9.3708 5.68194C9.90606 5.68194 10.3643 5.73145 10.7305 6.18497C11.5871 7.24522 10.6312 8.27413 9.5419 8.81876C8.7711 9.20431 8.40694 9.85763 8.40694 10.7673V12.8023H10.441V11.6028C10.441 9.77179 11.6188 10.232 12.4754 9.08683C13.6338 7.53761 13.3833 5.79122 11.8112 4.54698C10.013 3.12285 7.97803 3.8295 7.41084 4.21504C6.93946 4.53526 5.26953 5.72471 5.59063 8.2835L7.56729 8.28761C7.56729 8.28761 7.57139 8.04825 7.57139 7.57716H7.57198ZM10.4416 15.5861V13.4984H8.40752V15.5861H10.4416Z"
-                  fill="#26435F"
-                />
-              </svg>
+          </div>
+          <div className=" flex-1 w-full">
+            <div className="flex items-center justify-between">
+              <h1 className="text-[#26435F] text-sm font-semibold mb-1 text-[20px] pb-1  flex items-center justify-start">
+                Conceptual Accuracy
+                <div className="inline-block my-auto ml-[18.75px] cursor-pointer">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 19 19"
+                    fill="none"
+                  >
+                    <path
+                      d="M18.2844 9.64347C18.2844 14.5545 14.3032 18.536 9.39219 18.536C4.48115 18.536 0.5 14.5551 0.5 9.64347C0.5 4.73213 4.48086 0.750977 9.39219 0.750977C14.3032 0.750977 18.2844 4.73243 18.2844 9.64347ZM9.39219 2.73877C5.57862 2.73877 2.48721 5.83018 2.48721 9.64376C2.48721 13.457 5.57832 16.549 9.39219 16.549C13.2058 16.549 16.2972 13.457 16.2972 9.64376C16.2975 5.83047 13.2058 2.73877 9.39219 2.73877ZM7.57198 7.57716C7.57198 6.17442 8.47168 5.68194 9.3708 5.68194C9.90606 5.68194 10.3643 5.73145 10.7305 6.18497C11.5871 7.24522 10.6312 8.27413 9.5419 8.81876C8.7711 9.20431 8.40694 9.85763 8.40694 10.7673V12.8023H10.441V11.6028C10.441 9.77179 11.6188 10.232 12.4754 9.08683C13.6338 7.53761 13.3833 5.79122 11.8112 4.54698C10.013 3.12285 7.97803 3.8295 7.41084 4.21504C6.93946 4.53526 5.26953 5.72471 5.59063 8.2835L7.56729 8.28761C7.56729 8.28761 7.57139 8.04825 7.57139 7.57716H7.57198ZM10.4416 15.5861V13.4984H8.40752V15.5861H10.4416Z"
+                      fill="#26435F"
+                    />
+                  </svg>
+                </div>
+              </h1>
             </div>
-          </h1>
-        </div>
-        <div
-          id={styles.chartContainer}
-          className="!rounded-md w-full bg-white flex-1 shadow-[0px_0px_2.500001907348633px_0px_#00000040] custom-scroller pb-5 pl-4"
-        >
-          {accsubjects && accsubjects?.length > 0 ? (
-            <>
-              <div className="flex-1 relative z-[6000] flex justify-end translate-y-4 mr-3">
-                <InputSelectNew
-                  placeholder={""}
-                  parentClassName="ml-0  scale-[0.9] items-center flex text-[#FFA28D] text-xs border px-1 py-2 border-[#FFA28D] rounded-full  "
-                  inputContainerClassName=" my-0 py-[5px] px-[35px]"
-                  placeHolderClass="text-[#FFA28D] "
-                  labelClassname="text-sm"
-                  inputClassName="bg-transparent"
-                  value={selectedSubject}
-                  IconDemography={true}
-                  optionData={accsubjects.map((item) => item.subject)}
-                  onChange={(e) => handleSubjectChange(e)}
-                />
-                {/* <InputSelect
+            <div
+              id={styles.chartContainer}
+              className="!rounded-md w-full bg-white flex-1 shadow-[0px_0px_2.500001907348633px_0px_#00000040] custom-scroller pb-5 pl-4"
+            >
+              {accsubjects && accsubjects?.length > 0 ? (
+                <>
+                  <div className="flex-1 relative z-[6000] flex justify-end translate-y-4 mr-3">
+                    <InputSelectNew
+                      placeholder={""}
+                      parentClassName="ml-0  scale-[0.9] items-center flex text-[#FFA28D] text-xs border px-1 py-2 border-[#FFA28D] rounded-full  "
+                      inputContainerClassName=" my-0 py-[5px] px-[35px]"
+                      placeHolderClass="text-[#FFA28D] "
+                      labelClassname="text-sm"
+                      inputClassName="bg-transparent"
+                      value={selectedSubject}
+                      IconDemography={true}
+                      optionData={accsubjects.map((item) => item.subject)}
+                      onChange={(e) => handleSubjectChange(e)}
+                    />
+                    {/* <InputSelect
               value={selectedSubject}
               labelClassname="hidden"
               parentClassName="w-fit mr-5"
@@ -366,27 +364,27 @@ const SPFrame3 = ({ userDetail, isOwn, user }) => {
               optionData={subjects.map((item) => item.name)}
               onChange={(val) => handleSubjectChange(val)}
             /> */}
-                <RangeDate
-                  removeUnderline={true}
-                  className="ml-0 !font-normal"
-                  optionClassName="!w-min"
-                  inputContainerClassName="!w-min font-normal"
-                  handleRangeData={handleConceptAccuracy}
-                />
-              </div>
-              <StudentBubbleChart
-                subjects={accsubjects}
-                selectedSubject={selectedSubject}
-                accuracy={true}
-              />
-            </>
-          ) : (
-            <div
-              id="sconcept"
-              className=" w-full  z-[5000] min-h-[300px] rounded-md bg-white flex justify-center flex-col text-center items-center"
-            >
-              <div className="w-[70%] mx-auto   flex flex-col items-start">
-                {/* <button className="bg-[#FF7979] text-white rounded-md p-2 py-1 mb-3">
+                    <RangeDate
+                      removeUnderline={true}
+                      className="ml-0 !font-normal"
+                      optionClassName="!w-min"
+                      inputContainerClassName="!w-min font-normal"
+                      handleRangeData={handleConceptAccuracy}
+                    />
+                  </div>
+                  <StudentBubbleChart
+                    subjects={accsubjects}
+                    selectedSubject={selectedSubject}
+                    accuracy={true}
+                  />
+                </>
+              ) : (
+                <div
+                  id="sconcept"
+                  className=" w-full  z-[5000] min-h-[300px] rounded-md bg-white flex justify-center flex-col text-center items-center"
+                >
+                  <div className="w-[70%] mx-auto   flex flex-col items-start">
+                    {/* <button className="bg-[#FF7979] text-white rounded-md p-2 py-1 mb-3">
                   No Assignments Yet
                 </button>
                 <p className=" !whitespace-normal !text-left">
@@ -394,11 +392,13 @@ const SPFrame3 = ({ userDetail, isOwn, user }) => {
                   assignment is given, the student will be able to start it and
                   view detailed score reports through this table.
                 </p> */}
-              </div>
+                  </div>
+                </div>
+              )}
             </div>
-          )}
-        </div>
-      </div>
+          </div>
+        </>
+      }
     </div>
   );
 };
